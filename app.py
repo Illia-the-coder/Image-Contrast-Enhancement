2ce042d5b98997e3480dd18672c053c739f59583
illia Liudohovskyi <101904816+Illia-the-coder@users.noreply.github.com>
2024-07-02 20:13:06 +0200

    _

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors
 import imageio.v2 as imageio
-from PIL import Image
 import scipy.signal
 import scipy.sparse
 import scipy.optimize
 import cv2
-import sys
+from PIL import Image
+import gradio as gr
 
+# Ying Algorithm
 def computeTextureWeights(fin, sigma, sharpness):
     dt0_v = np.vstack((np.diff(fin, n=1, axis=0), fin[0,:]-fin[-1,:]))
     dt0_h = np.vstack((np.diff(fin, n=1, axis=1).conj().T, fin[:,0].conj().T-fin[:,-1].conj().T)).conj().T
@@ -136,12 +138,122 @@ def Ying_2017_CAIP(img, mu=0.5, a=-0.3293, b=1.1258):
     result[result<0] = 0
     return result.astype(np.uint8)
 
-def main():
-    img_name = sys.argv[1]
-    img = imageio.imread(img_name)
-    result = Ying_2017_CAIP(img)
-    plt.imshow(result)
-    plt.show()
+# DHE Algorithm
+def build_is_hist(img):
+    hei = img.shape[0]
+    wid = img.shape[1]
+    ch = img.shape[2]
+    Img = np.zeros((hei+4, wid+4, ch))
+    for i in range(ch):
+        Img[:,:,i] = np.pad(img[:,:,i], (2,2), 'edge')
+    hsv = (matplotlib.colors.rgb_to_hsv(Img))
+    hsv[:,:,0] = hsv[:,:,0] * 255
+    hsv[:,:,1] = hsv[:,:,1] * 255
+    hsv[hsv>255] = 255
+    hsv[hsv<0] = 0
+    hsv = hsv.astype(np.uint8).astype(np.float64)
+    fh = np.array([[-1.0,0.0,1.0],[-2.0,0.0,2.0],[-1.0,0.0,1.0]])
+    fv = fh.conj().T
+    
+    H = hsv[:,:,0]
+    S = hsv[:,:,1]
+    I = hsv[:,:,2]
 
-if __name__ == '__main__':
-    main()
+    dIh = scipy.signal.convolve2d(I, np.rot90(fh, 2), mode='same')
+    dIv = scipy.signal.convolve2d(I, np.rot90(fv, 2), mode='same')
+    dIh[dIh==0] = 0.00001
+    dIv[dIv==0] = 0.00001
+    dI = np.sqrt(dIh**2+dIv**2).astype(np.uint32)
+    di = dI[2:hei+2,2:wid+2]
+    
+    dSh = scipy.signal.convolve2d(S, np.rot90(fh, 2), mode='same')
+    dSv = scipy.signal.convolve2d(S, np.rot90(fv, 2), mode='same')
+    dSh[dSh==0] = 0.00001
+    dSv[dSv==0] = 0.00001
+    dS = np.sqrt(dSh**2+dSv**2).astype(np.uint32)
+    ds = dS[2:hei+2,2:wid+2]
+
+    
+    h = H[2:hei+2,2:wid+2]
+    s = S[2:hei+2,2:wid+2]
+    i = I[2:hei+2,2:wid+2].astype(np.uint8)
+    
+    Imean = scipy.signal.convolve2d(I, np.ones((5,5))/25, mode='same')
+    Smean = scipy.signal.convolve2d(S, np.ones((5,5))/25, mode='same')
+    
+    Rho = np.zeros((hei+4,wid+4))
+    for p in range(2,hei+2):
+        for q in range(2,wid+2):
+            tmpi = I[p-2:p+3,q-2:q+3]
+            tmps = S[p-2:p+3,q-2:q+3]
+            corre = np.corrcoef(tmpi.flatten('F'), tmps.flatten('F'))
+            Rho[p,q] = corre[0,1]
+    
+    rho = np.abs(Rho[2:hei+2,2:wid+2])
+    rho[np.isnan(rho)] = 0
+    rd = (rho*ds).astype(np.uint32)
+    Hist_I = np.zeros((256,1))
+    Hist_S = np.zeros((256,1))
+    
+    for n in range(0,255):
+        temp = np.zeros(di.shape)
+        temp[i==n] = di[i==n]
+        Hist_I[n+1] = np.sum(temp.flatten('F'))
+        temp = np.zeros(di.shape)
+        temp[i==n] = rd[i==n]
+        Hist_S[n+1] = np.sum(temp.flatten('F'))
+
+    return Hist_I, Hist_S
+
+def dhe(img, alpha=0.5):
+    
+    hist_i, hist_s = build_is_hist(img)
+    hist_c = alpha*hist_s + (1-alpha)*hist_i
+    hist_sum = np.sum(hist_c)
+    hist_cum = hist_c.cumsum(axis=0)
+    
+    hsv = matplotlib.colors.rgb_to_hsv(img)
+    h = hsv[:,:,0]
+    s = hsv[:,:,1]
+    i = hsv[:,:,2].astype(np.uint8)
+    
+    c = hist_cum / hist_sum
+    s_r = (c * 255)
+    i_s = np.zeros(i.shape)
+    for n in range(0,255):
+        i_s[i==n] = s_r[n+1]/255.0
+    i_s[i==255] = 1
+    hsi_o = np.stack((h,s,i_s), axis=2)
+    result = matplotlib.colors.hsv_to_rgb(hsi_o)
+    
+    result = result * 255
+    result[result>255] = 255
+    result[result<0] = 0
+    return result.astype(np.uint8)
+
+def process_image(image, algorithm):
+    if algorithm == "Ying":
+        return Ying_2017_CAIP(image)
+    elif algorithm == "DHE":
+        return dhe(image)
+    else:
+        return image
+
+# Gradio app
+algorithm_options = ["Ying", "DHE"]
+app = gr.Interface(
+    fn=process_image,
+    inputs=[
+        gr.Image(type="numpy", label="Input Image"),
+        gr.Radio(algorithm_options, label="Algorithm")
+    ],
+    outputs=gr.Image(type="numpy", label="Output Image"),
+    title="Image Contrast Enhancement",
+    description="Apply 'A New Image Contrast Enhancement Algorithm Using Exposure Fusion Framework' or 'A Dynamic Histogram Equalization for Image Contrast Enhancement' to an input image.",
+    examples=[
+        ["testdata/01.jpg", "Ying"],
+        ["testdata/02.jpg", "DHE"]
+    ]
+)
+
+app.launch()
